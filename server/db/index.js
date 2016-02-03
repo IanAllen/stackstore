1fbd4b21ce46634199be14945dc29a8681bb52cf
IanAllen <ianavilallen@gmail.com>
2016-02-02 19:27:13 -0500

    touchups

--- server/db/index.js
+++ server/db/index.js
@@ -4,10 +4,10 @@ var path = require('path');
 var chalk = require('chalk');
 
 var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
-
+console.log(DATABASE_URI)
 var mongoose = require('mongoose');
+console.log(mongoose)
 var db = mongoose.connect(DATABASE_URI).connection;
-
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
